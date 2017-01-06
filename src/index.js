3dcbcd41a67ba642954fd8ea37d30bb286013540
Jesse Buchanan <hello@jessebuchanan.ca>
2017-01-06 15:48:05 -0500

    demonstrate issue with eslint-plugin-flowtype-errors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
+// @flow
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+
+import { GraphQLObjectType } from 'graphql'
+import type { GraphQLOutputType } from 'graphql'
+
+// The following line (12) is detected as a flow error by the plugin and flow cli
+const x: number = "lol"
+
+// The following line (15) is detected as a flow error by the flow CLI, but not the plugin
+const y: GraphQLOutputType = new GraphQLObjectType({
+  name: 'foo'
+  // fields: (missing, flow should complain)
+})
+
+console.log(x, y)
 
 ReactDOM.render(
   <App />,
